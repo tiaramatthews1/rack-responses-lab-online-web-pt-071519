fab3ad6d0f8792e0916e782c3afb907c64dde62d
Tiara Matthews <tiaramatthews96@yahoo.com>
2019-09-23 15:54:31 +0000

    Automatically backed up by Learn

--- spec/rack_mechanics_spec.rb
+++ spec/rack_mechanics_spec.rb
@@ -2,7 +2,7 @@ require_relative './spec_helper'
 
 describe "App" do
 
-  it 'wishes user a good morning in the morning' do
+  xit 'wishes user a good morning in the morning' do
     allow(Time).to receive(:now){
       Time.new(2015,11,27,9,30)
     }
@@ -10,7 +10,7 @@ describe "App" do
     expect(last_response.body).to include("Morning")
   end
 
-  it 'wishes user a good afternoon in the afternoon' do
+  xit 'wishes user a good afternoon in the afternoon' do
     allow(Time).to receive(:now){
       Time.new(2015,11,27,14,30)
     }
